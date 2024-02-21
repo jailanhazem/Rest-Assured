2ec18dccc2c2e878fe57287f3771c1a6f564c8d7
jailanhazem <120454805+jailanhazem@users.noreply.github.com>
2024-02-21 14:43:44 +0200

    Add files via upload

---
+++ RestAssured/src/main/java/Requests.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Requests {
+
+    public static int getRandomNumber()
+    {
+        return new Random().nextInt(10)+1;
+    }
+
+
+}
---
+++ RestAssured/src/test/java/TestCases.java
@@ -0,0 +1,70 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static io.restassured.RestAssured.given;
+
+public class TestCases {
+
+        private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+        private static final String USERS_ENDPOINT = "/users";
+        private static final String POSTS_ENDPOINT = "/posts";
+        private static int USER_ID;
+
+        @BeforeClass
+        public static void setup() {
+            RestAssured.baseURI = BASE_URL;
+            USER_ID = Requests.getRandomNumber();
+        }
+
+        @Test
+        public void getUserEmail() {
+
+            Response r = given().pathParam("userId", USER_ID).get(USERS_ENDPOINT+"/{userId}");
+            JsonPath responseBody = r.getBody().jsonPath();
+            System.out.println(responseBody.get("email").toString());
+        }
+
+        @Test
+        public void getUserPosts() {
+            Response r =given().queryParam("userId",USER_ID ).get(POSTS_ENDPOINT);
+            Assert.assertTrue(checkUserId(r.jsonPath()));
+            Assert.assertTrue(checkPostId(r.jsonPath()));
+            r.prettyPrint();
+        }
+
+
+        public static boolean checkUserId(JsonPath jsonPath)
+        {
+            boolean flag =true;
+            for (Object s:jsonPath.getList("userId"))
+                if (!s.equals(USER_ID)) {
+                    flag = false;
+                    break;
+                }
+            return flag;
+        }
+
+        public static boolean checkPostId(JsonPath jsonPath)
+        {
+
+            boolean flag =true;
+            for (Object s:jsonPath.getList("id"))
+                if (!((int)s >= 1 && (int)s <= 100)) {
+                    flag = false;
+                    break;
+                }
+            return flag;
+        }
+
+    }
+
+
+
+
+
